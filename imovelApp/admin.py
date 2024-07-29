410cee27bdbb76483f3b114c7d8ecd3e25ffb6b0
gbrl08snts <gabriel.aquino.069@ufrn.edu.br>
2024-07-29 17:31:54 -0300

    Criando Tabelas do Banco de Dados

--- imovelApp/admin.py
+++ imovelApp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from imovelApp import models
+from imovelApp.models import Client
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(models.RegisterLocation)
+
+
+# admin.site.register(models.Immobile)
+# admin.site.register(models.ImmobileImage)
+
+
+class ImmobileImageInlineAdmin(admin.TabularInline):
+    model = models.ImmobileImage
+    extra = 0
+
+
+class ImmobileAdmin(admin.ModelAdmin):
+    inlines = [ImmobileImageInlineAdmin]
+
+
+admin.site.register(models.Immobile, ImmobileAdmin)
